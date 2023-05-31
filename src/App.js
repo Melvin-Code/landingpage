0a17509f5d4cb7aa5c185f61db9331cdf9dfda9d
Melvin-Code <melvingonzalez470@gmail.com>
2023-05-31 19:07:17 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { Box, Paper } from "@mui/material";
+
+function App() {
+  return (
+    <Paper
+      sx={{
+        background: "#f4f8fa",
+        width: "100vw",
+        height: "100vh",
+        position: "relative",
+      }}
+    >
+      <Box
+        sx={{
+          background: "#f4f8fa",
+          padding: "4px",
+          position: "absolute",
+          boxShadow:
+            "0 4px 8px 0 rgba(53,105,128,.3), 0 6px 20px 0 rgba(165,200,213,.41)",
+          margin: "0 auto",
+          marginTop: "100px",
+          width: "800",
+          height: "400",
+        }}
+      >
+        hhhhhhlfdgfg
+      </Box>
+    </Paper>
+  );
+}
+
+export default App;
