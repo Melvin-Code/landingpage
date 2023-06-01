a85383ae7f702a4d48d94d0e383a00ef67646a6f
Melvin Gonzalez <57917756+Melvin-Code@users.noreply.github.com>
2023-06-01 01:31:56 +0000

    Update App.js and index.js

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ function App() {
         width: "100vw",
         height: "100vh",
         position: "relative",
+        display: "flex",
+        justifyContent: "center",
       }}
     >
       <Box
         sx={{
           background: "#f4f8fa",
-          padding: "4px",
-          position: "absolute",
+          padding: "4rem",
           boxShadow:
             "0 4px 8px 0 rgba(53,105,128,.3), 0 6px 20px 0 rgba(165,200,213,.41)",
-          margin: "0 auto",
           marginTop: "100px",
-          width: "800",
+          width: "80",
           height: "400",
         }}
       >
-        hhhhhhlfdgfg
+        hhhhhhlfdgfglll
       </Box>
     </Paper>
   );
